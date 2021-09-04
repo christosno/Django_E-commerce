6de39229d346ecb7a97c448af087bfecf8461f60
Christos Nounis <christosno1984@gmail.com>
2021-09-04 22:30:22 +0300

    final commit

--- ecomsite/shop/admin.py
+++ ecomsite/shop/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Products
+from .models import Products, Order
 # Register your models here.
 
+admin.site.site_header = "E-commerce site"
+admin.site.title = "EBC shop"
+admin.site.index_title = "Manage EBC shoping"
+
 admin.site.register(Products)
+admin.site.register(Order)
